6ea9fa529497e9463ee6d72a1d134e228870101f
Vlad Grigo <vladgrigorita2001@gmail.com>
2022-06-19 20:14:22 +0300

    UI changes to mobile form of profile

--- MaSTMVC/views/layouts/profile.php
+++ MaSTMVC/views/layouts/profile.php
@@ -31,7 +31,7 @@
         <i class="fas fa-bars"></i>
     </label>
         <ul>
-            <li><a href="/MaSTMVC/index.php/">Home</a></li>
+            <li><a  href="/MaSTMVC/index.php/">Home</a></li>
             <li><a class="active" href="/MaSTMVC/index.php/profile">Profile</a></li>
             <li><a href="/MaSTMVC/index.php/catalogue">Catalogue</a></li>
             <li><a href="/MaSTMVC/index.php/logout">Log out</a></li>
